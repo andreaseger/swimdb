581aac63d4b7a91e92df0bdb1aaf9eb5ae0369a0
sch1zo <andreas.eger.reg@googlemail.com>
2010-09-29 17:51:01 +0200

    forgot to add the spec changes
    Signed-off-by: sch1zo <andreas.eger.reg@googlemail.com>

--- spec/controllers/schedules_controller_spec.rb
+++ spec/controllers/schedules_controller_spec.rb
@@ -23,6 +23,105 @@ describe SchedulesController do
   end
 
 
+  describe "GET new" do
+    it "assigns a new schedule as @schedule" do
+      Schedule.stub(:new) { mock_schedule }
+      get :new
+      assigns(:schedule).should be(mock_schedule)
+    end
+  end
+
+  describe "GET edit" do
+    it "assigns the requested schedule as @schedule" do
+      Schedule.stub(:find).with("37") { mock_schedule }
+      get :edit, :id => "37"
+      assigns(:schedule).should be(mock_schedule)
+    end
+  end
+
+  describe "POST create" do
+
+    describe "with valid params" do
+      it "assigns a newly created schedule as @schedule" do
+        Schedule.stub(:new).with({'these' => 'params'}) { mock_schedule(:save => true) }
+        post :create, :schedule => {'these' => 'params'}
+        assigns(:schedule).should be(mock_schedule)
+      end
+
+      it "redirects to the created schedule" do
+        Schedule.stub(:new) { mock_schedule(:save => true) }
+        post :create, :schedule => {}
+        response.should redirect_to(schedule_url(mock_schedule))
+      end
+    end
+
+    describe "with invalid params" do
+      it "assigns a newly created but unsaved schedule as @schedule" do
+        Schedule.stub(:new).with({'these' => 'params'}) { mock_schedule(:save => false) }
+        post :create, :schedule => {'these' => 'params'}
+        assigns(:schedule).should be(mock_schedule)
+      end
+
+      it "re-renders the 'new' template" do
+        Schedule.stub(:new) { mock_schedule(:save => false) }
+        post :create, :schedule => {}
+        response.should render_template("new")
+      end
+    end
+
+  end
+
+  describe "PUT update" do
+
+    describe "with valid params" do
+      it "updates the requested schedule" do
+        Schedule.should_receive(:find).with("37") { mock_schedule }
+        mock_schedule.should_receive(:update_attributes).with({'these' => 'params'})
+        put :update, :id => "37", :schedule => {'these' => 'params'}
+      end
+
+      it "assigns the requested schedule as @schedule" do
+        Schedule.stub(:find) { mock_schedule(:update_attributes => true) }
+        put :update, :id => "1"
+        assigns(:schedule).should be(mock_schedule)
+      end
+
+      it "redirects to the schedule" do
+        Schedule.stub(:find) { mock_schedule(:update_attributes => true) }
+        put :update, :id => "1"
+        response.should redirect_to(schedule_url(mock_schedule))
+      end
+    end
+
+    describe "with invalid params" do
+      it "assigns the schedule as @schedule" do
+        Schedule.stub(:find) { mock_schedule(:update_attributes => false) }
+        put :update, :id => "1"
+        assigns(:schedule).should be(mock_schedule)
+      end
+
+      it "re-renders the 'edit' template" do
+        Schedule.stub(:find) { mock_schedule(:update_attributes => false) }
+        put :update, :id => "1"
+        response.should render_template("edit")
+      end
+    end
+
+  end
+
+  describe "DELETE destroy" do
+    it "destroys the requested schedule" do
+      Schedule.should_receive(:find).with("37") { mock_schedule }
+      mock_schedule.should_receive(:destroy)
+      delete :destroy, :id => "37"
+    end
+
+    it "redirects to the schedules list" do
+      Schedule.stub(:find) { mock_schedule }
+      delete :destroy, :id => "1"
+      response.should redirect_to(schedules_url)
+    end
+  end
 
 
 #  describe 'index' do
